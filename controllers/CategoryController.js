522b005c34b4e05ed75848ede45328fdd5930c84
xkendx <63884610+xkendx@users.noreply.github.com>
2020-12-16 00:27:48 +0300

    Update CategoryController.js

--- controllers/CategoryController.js
+++ controllers/CategoryController.js
@@ -1,7 +1,72 @@
 const Category = require("../models/Category");
+const { validationResult } = require("express-validator");
 
 exports.addCategory = async (req, res) => {
-  const { CategoryName, description } = req.body;
+  try {
+    const { categoryName, description } = req.body;
 
-  
+    //fied validation
+    const validationErr = validationResult(req);
+    if (validationErr?.errors?.length > 0) {
+      return res.status(400).json({ errors: validationErr.array() });
+    }
+
+    // category exist check
+    const existCategory = await Category.findOne({
+      categoryName: categoryName,
+    });
+    if (existCategory) {
+      return res
+        .status(400)
+        .json({ errors: [{ message: "Category already exists" }] });
+    }
+
+    //save category
+    const category = new Category({
+      categoryName: categoryName,
+      description: description,
+    });
+
+    // const category = new Category(req.body);
+    const addedCategory = await category.save({ new: true });
+    // res.status(200).send('Category added');
+    res.status(200).json(addedCategory);
+  } catch (error) {
+    res.status(500).json({ errors: [{ message: error.message }] });
+  }
 };
+
+exports.getCategory = async (req, res) => {
+  try {
+    const category = await Category.findById({ __id: req.params.id });
+    res.status(200).json(category);
+  } catch (error) {
+    return res.status(500).json({ errors: [{ message: error.message }] });
+  }
+};
+
+exports.updateCategory = async (req, res) => {
+  try {
+    // validation
+    const validationErr = validationResult(req);
+    if (validationErr?.errors?.length > 0) {
+      return res.status(400).json({ errors: validationErr.array() });
+    }
+
+    // update
+    const updatedCategory = await Category.findOneAndUpdate(
+      { __id: req.body.id },
+      { ...req.body, status: "updated", updatedDate: Date.now() },
+      { new: true, runValidators: true }
+    );
+
+    // res.status(200).send("Category updated");
+    res.status(200).json(updatedCategory);
+  } catch (error) {
+    return res.status(500).json({ errors: [{ message: error.message }] });
+  }
+};
+
+exports.deleteCategory = async (req, res) => {};
+
+exports.getCategories = async (req, res) => {};

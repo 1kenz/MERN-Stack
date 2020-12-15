fc566ce9b11d1b8375f047be5cd9faa41eefcacd
xkendx <63884610+xkendx@users.noreply.github.com>
2020-12-15 23:08:12 +0300

    Update CategoryRouter.js

--- routers/CategoryRouter.js
+++ routers/CategoryRouter.js
@@ -1,16 +1,24 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const CategoryController = require('../controllers/CategoryController');
+const CategoryController = require("../controllers/CategoryController");
+const validations = require("../middleware/validationMiddleware");
 
+router.post(
+  "/addCategory",
+  validations.categoryValidation,
+  CategoryController.addCategory
+);
 
-router.post('/addCategory', CategoryController.addCategory);
+router.get("/getCategory/:id", CategoryController.getCategory);
 
-router.get('/getCategory/:id', CategoryController.getCategory);
+router.post(
+  "/updateCategory",
+  validations.categoryValidation,
+  CategoryController.updateCategory
+);
 
-router.post('/updateCategory', CategoryController.updateCategory);
+router.get("/deleteCategory/:id", CategoryController.deleteCategory);
 
-router.get('/deleteCategory/:id', CategoryController.deleteCategory);
-
-router.get('/', CategoryController.getCategories);
+router.get("/", CategoryController.getCategories);
 
 module.exports = router;

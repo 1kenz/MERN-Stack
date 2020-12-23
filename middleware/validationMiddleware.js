dd45ea934e0390409b9ebc3f23dcbf9deeafdf81
xkendx <63884610+xkendx@users.noreply.github.com>
2020-12-23 22:10:01 +0300

    feat: clean code

--- middleware/validationMiddleware.js
+++ middleware/validationMiddleware.js
@@ -1,11 +1,6 @@
-const { check } = require("express-validator");
+const { check } = require('express-validator');
 
 exports.categoryValidation = [
-  check(
-    "categoryName",
-    "Please enter a name minumum 2 and maximum 20 chars"
-  ).isLength({ min: 2, max: 20 }),
-  check("description", "Please enter a namemaximum 200 chars").isLength({
-    max: 200,
-  }),
-];
+    check('categoryName', "Please enter a name minumum 2 and maximum 20 chars").isLength({ min: 2, max: 20 }),
+    check('description', "Please enter a namemaximum 200 chars").isLength({ max: 200 }),
+]
